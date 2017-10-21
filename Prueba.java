d088cca09ee7e588e152f90e49780c3a6713c310
adrianjguerrero <32990164+adrianjguerrero@users.noreply.github.com>
2017-10-21 19:48:45 -0400

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,13 @@
+
+package prueba;
+import javax.swing.JOptionPane;
+
+public class Prueba {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Probando probando sonido 1 2 3");
+        //Crea otro mensaje para probar el sonido y un comentario
+    }
+    
+}
