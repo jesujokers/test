9d4038a900b3eadfbb263c13fc3c841a2bfeca10
jesujokers <32984153+jesujokers@users.noreply.github.com>
2017-10-21 21:01:33 -0400

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -7,7 +7,8 @@ public class Prueba {
     
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Probando probando sonido 1 2 3");
-        //Crea otro mensaje para probar el sonido y un comentario
-    }
+        JOptionPane.showMessageDialog(null,"AAAAAAAAAAAAAAAAAHHHHHH" );
+    
+        }  
     
 }
